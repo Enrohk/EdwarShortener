558f72860e5edd102c50b207f451a3f24fa54a91
Ramón José Arellano <ramonmoxo@gmail.com>
2016-05-21 10:43:15 +0200

    Add project files.

---
+++ EdwardShortener/modules/HomeModule.cs
@@ -0,0 +1,21 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EdwardShortener.modules
+{
+    public class HomeModule : NancyModule
+    {
+
+        public HomeModule ()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["Index"];
+            };
+        }
+
+    }
+}
