27cf3dc6d908148deec5b0bb9695ec4969d4203d
Ramón José Arellano <ramonmoxo@gmail.com>
2016-06-16 20:01:41 +0200

    added db connectcion string and testing connection works

---
+++ EdwardShortener.Tests/ConnectionTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using EdwardShortener.Objects;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Configuration;
+using Dapper;
+
+namespace EdwardShortener.Tests
+{
+    [TestClass]
+    public class ConnectionTest
+    {
+        [TestMethod]
+        public void ConnectionDhouldWorkTest()
+        {
+            string con = Properties.Settings.Default["EdwardShortenerDbCS"].ToString();
+            using (SqlConnection myConnection = new SqlConnection(con))
+            {
+                string query = "SELECT * FROM [dbo].[SQL_Function_ShortedURIListByTime] ()";
+                IEnumerable<UrlObject> v_result = myConnection.Query<UrlObject>(query);
+                Assert.IsTrue(v_result.ToList().Count > 0);
+            }
+        }
+    }
+}
